969dff9c8994761f1bdd0a1111e47b0cbc6aa023
tkrach <98433403+tkrach@users.noreply.github.com>
2024-05-11 15:08:09 +0900

    Time: 19 ms (10.54%), Space: 22.7 MB (88.17%)

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,23 +2,8 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int flowerCount = 0;
-        //Initial For loop to interate throught he flowerbed
         for (int i = 0; i < flowerbed.size(); i++) {
-            //Edge Case Beginning
-            if (i == 0 && flowerbed[i] == 0 && (i + 1 == flowerbed.size() || flowerbed[i+1] == 0)){
-                //Increment count and place a flower
-                flowerCount++;
-                flowerbed[i] = 1;
-            }
-            //Edge Case End
-            else if (i == flowerbed.size() - 1 && flowerbed[i] == 0 && flowerbed[i-1] == 0){
-                //Increment count and place a flower
-                flowerCount++;
-                flowerbed[i] = 1;
-            }
-            //Normal Case
-            else if ((i != 0 && i != flowerbed.size() - 1) && flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0) {
-                //Iterate and place flower
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i+1] == 0)) {
                 flowerCount++;
                 flowerbed[i] = 1;
             }
