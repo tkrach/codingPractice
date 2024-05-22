63144083104c2404973ed87dd47c1089a873787e
tkrach <98433403+tkrach@users.noreply.github.com>
2024-05-22 20:51:30 +0900

    Time: 12 ms (80.76%), Space: 21.6 MB (37.19%)

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //2 Pointer problem lets go
+        //Just do an initial for loop to put everything in its place
         int nonZero = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
                 nums[nonZero++] = nums[i];
             }
         }
+        //fill in the zeros! 
         for (int j = nonZero; j < nums.size(); j++) {
             nums[j] = 0;
         }
