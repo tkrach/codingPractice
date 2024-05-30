5d2001d2cb08b81da1b8276f8ee2efb42d920400
tkrach <98433403+tkrach@users.noreply.github.com>
2024-05-30 12:54:41 +0900

    Time: 0 ms (100%), Space: 11.1 MB (6.59%)

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int j = 0, i = m; j < n; j++){
-            nums1[i] = nums2[j];
-            i++;
+        //nums1 pointer
+        int i = m - 1;
+        //nums2 pointer
+        int j = n - 1;
+        //Very ened of the array
+        int k = m + n - 1;
+        while (j >= 0) {
+            //If nums1 element is larger, we put it at the end and decrement
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }
+            //Else nums2 is bigger so we put that at the end and decrement
+            else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        //Just use built in sort method
-        sort(nums1.begin(),nums1.end());
     }   
 };
