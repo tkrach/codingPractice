8025c320d347a75b7fec7300a85fbf55f94bcbc1
tkrach <98433403+tkrach@users.noreply.github.com>
2024-07-08 12:01:34 +0900

    Time: 18 ms (42.94%), Space: 11.2 MB (34.37%)

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        // Map problem again! 
+        std::unordered_map<char, int> charCount;
+        for (char c : magazine) {
+            charCount[c]++;
+        }
+        
+        for (char c : ransomNote) {
+            if (charCount[c] > 0) {
+                charCount[c]--; 
+            } else {
+                return false;
+            }
+        }
+
+        return true;  
+    }
+};
