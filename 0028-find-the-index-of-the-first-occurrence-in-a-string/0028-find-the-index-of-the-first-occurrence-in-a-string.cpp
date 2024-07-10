37cae0cfc0b02afddfe4eb76a59ba67a72803f53
tkrach <98433403+tkrach@users.noreply.github.com>
2024-07-10 11:18:40 +0900

    Time: 3 ms (43.31%), Space: 7.4 MB (95.51%)

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        // Just use built in find function lol 
         size_t found = haystack.find(needle);
         return found; 
     }
