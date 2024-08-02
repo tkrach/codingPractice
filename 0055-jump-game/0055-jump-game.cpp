42a315aca40f0f57eb30468461f2291c4b35be78
tkrach <98433403+tkrach@users.noreply.github.com>
2024-08-02 15:02:39 +0900

    Time: 38 ms (84.78%), Space: 51.1 MB (42.14%)

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,7 @@ public:
     bool canJump(vector<int>& nums) {
         int furthest = 0;
         
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); i++) {
             // If the current index is beyond the furthest reachable point, return false
             if (i > furthest) {
                 return false;
